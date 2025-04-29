b590dad5dd0c816eb9f4fae9b7244e8ef3d4ed70
VLira96 <lira94634@gmail.com>
2025-04-29 14:17:20 -0300

    Diminuindo o mapa de competição. De 8x8 para 6x6.

--- main-competicao.py
+++ main-competicao.py
@@ -1,6 +1,6 @@
 from agentes.competicao import competicao
 
-agentes = competicao((0, 0), (7, 7), "Sergio", "Moacir")
+agentes = competicao((0, 0), (5, 5), "Sergio", "Moacir")
 
 print(f"\n\n\t{agentes[0]}\n")
 print(f"\t{agentes[1]}\n")

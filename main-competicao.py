fed5c5080684e88980f8986240b2577f4409f792
VLira96 <lira94634@gmail.com>
2025-04-30 13:50:03 -0300

    Alterando a posição de inicio dos agentes do arquivo de competição

--- main-competicao.py
+++ main-competicao.py
@@ -1,6 +1,6 @@
 from agentes.competicao import competicao
 
-agentes = competicao((0, 0), (5, 5), "Sergio", "Moacir")
+agentes = competicao((1, 1), (4, 4), "Sergio", "Moacir")
 
 print(f"\n\n\t{agentes[0]}\n")
 print(f"\t{agentes[1]}\n")

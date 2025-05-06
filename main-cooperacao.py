dd568f670f79be896c47494bbe486ba873b3ec6d
VLira96 <lira94634@gmail.com>
2025-05-06 17:07:06 -0300

    deixando agentes mais proximos do centro

--- main-cooperacao.py
+++ main-cooperacao.py
@@ -1,6 +1,6 @@
 from agentes.cooperacao import cooperacao
 
-agentes = cooperacao((0, 0), (7, 7), "Anderson", "Ivo")
+agentes = cooperacao((1, 1), (6, 6), "Ruben", "Ivo")
 
 print(f"\n\n\t{agentes[0].coletas}\n")
 print(f"\t{agentes[1].coletas}\n")
